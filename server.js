8b100b3c554c9e92f30c79f2153b4650b8d94450
NagaSatishArigela <nagasatisharigela@gmail.com>
2024-05-14 13:44:05 +0530

    package update

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require("express");
-const mongoose = require('mongoose')
 const errorHandler = require("./middleWare/errorHandler");
-const dotenv = require("dotenv").config();
 const app = express();
 const mySql = require('./mySqlDb');
 // const url = 'mongodb://localhost:27017/blogsDB'
 const port = process.env.port || 5000;
-const cors = require('express-cors');
+const cors = require('cors');
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ limit: '10mb' }));
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use("/api/blogs", require("./routes/blogsRoute"));
-app.use("/api/user", require("./routes/userRoute"));
 app.use(errorHandler)
 app.listen(port, () => {
   console.log(`I am in exprees server on port ${port}`);
